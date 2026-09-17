1679f8c0c1b135c22d1239c6ef42b003fbb26616
zetharionn <127628065+zetharionn@users.noreply.github.com>
2026-09-17 18:10:24 +0300

    refactor(setup): simplified, removed unnecessary checks, removed asynchrony when it's not needed

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,9 @@
-import { TelegramClient } from 'teleproto'
-import { StringSession } from 'teleproto/sessions'
+import { log } from '@clack/prompts'
 
-import { getCredentials } from './credentials.ts'
 import { setup } from './setup.ts'
-import { logger } from './utils/logger.ts'
 
-let credentials = await getCredentials()
-
-if (!credentials) {
-	await setup()
-	credentials = await getCredentials()
-}
-
-if (!credentials) {
-	process.exit(1)
-}
-
-const client = new TelegramClient(
-	new StringSession(credentials.session),
-	Number(credentials.api_id),
-	credentials.api_hash,
-	{
-		baseLogger: logger
-	}
-)
+const client = await setup()
 
 await client.connect()
 
-console.log('Signed in as', (await client.getMe()).username)
+log.success(`Signed in as: ${(await client.getMe()).username}`)
--- src/setup.ts
+++ src/setup.ts
@@ -1,47 +1,45 @@
-import { intro, log, outro, password, text } from '@clack/prompts'
+import { log, password, text } from '@clack/prompts'
 import { TelegramClient } from 'teleproto'
 import { StringSession } from 'teleproto/sessions'
 
-import { setCredentials } from './credentials.ts'
+import { getCredentials, setCredentials } from './credentials.ts'
 import { ask } from './utils/ask.ts'
 import { logger } from './utils/logger.ts'
 
-export const setup = async (): Promise<void> => {
-	intro("Let's set everything up")
+export const setup = async () => {
+	const credentials = await getCredentials()
 
-	log.message(
-		"We need to get API credentials, if you haven't already got them, visit my.telegram.org"
-	)
+	if (!credentials) {
+		log.message(
+			"We need to get API credentials, if you haven't already got them, visit my.telegram.org"
+		)
 
-	const api_id = await ask(async () => text({ message: 'API ID', placeholder: '00000000' }))
-	const api_hash = await ask(
-		async () => await text({ message: 'API HASH', placeholder: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' })
-	)
-
-	try {
-		log.info('Authorize using phone number linked to your Telegram account')
+		const api_id = await ask(() => text({ message: 'API ID', placeholder: '00000000' }))
+		const api_hash = await ask(() =>
+			text({ message: 'API HASH', placeholder: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' })
+		)
 
 		const client = new TelegramClient(new StringSession(''), Number(api_id), api_hash, {
 			baseLogger: logger
 		})
 
 		await client.start({
-			phoneNumber: async () =>
-				ask(async () =>
+			phoneNumber: () =>
+				ask(() =>
 					text({
 						message: 'Phone number',
 						placeholder: '+0000000000'
 					})
 				),
-			phoneCode: async () =>
-				ask(async () =>
+			phoneCode: () =>
+				ask(() =>
 					password({
 						message: 'Login code',
 						mask: '*'
 					})
 				),
-			password: async hint =>
-				ask(async () =>
+			password: hint =>
+				ask(() =>
 					password({
 						message: `2FA password ${hint ? `(${hint})` : ''}`,
 						mask: '*'
@@ -52,11 +50,15 @@ export const setup = async (): Promise<void> => {
 
 		await setCredentials({ session: client.session.save(), api_id, api_hash })
 
-		await client.disconnect()
-
-		outro('Everything is up')
-	} catch (error) {
-		log.error(String(error))
-		outro('Please try again')
+		return client
 	}
+
+	return new TelegramClient(
+		new StringSession(credentials?.session),
+		Number(credentials?.api_id),
+		credentials?.api_hash,
+		{
+			baseLogger: logger
+		}
+	)
 }
