947c668d7b4fe365ddef096d3c3a0c5af0bff51d
zetharionn <127628065+zetharionn@users.noreply.github.com>
2026-09-17 16:47:04 +0300

    refactor(credentials): simplify logic

--- src/credentials.ts
+++ src/credentials.ts
@@ -5,21 +5,21 @@ interface Credentials {
 }
 
 const service = 'telesweeper'
-const names = ['session', 'api_id', 'api_hash'] as const
 
 export const getCredentials = async () => {
-	const credentials: Credentials = Object.fromEntries(
+	const names = ['session', 'api_id', 'api_hash'] as const
+	const { session, api_id, api_hash } = Object.fromEntries(
 		await Promise.all(names.map(async name => [name, await Bun.secrets.get({ service, name })]))
 	)
 
-	if (!credentials?.session || !credentials?.api_id || !credentials?.api_hash) return null
-	return credentials
+	if (!session || !api_id || !api_hash) return null
+	return { session, api_id, api_hash }
 }
 
-export const setCredentials = async (session: string, api_id: string, api_hash: string) => {
-	const values = [session, api_id, api_hash] as const
-	const pairs = names.map((name, index) => ({ name, value: values[index]! }))
+export const setCredentials = async (credentials: Credentials) => {
 	await Promise.all(
-		pairs.map(async ({ name, value }) => await Bun.secrets.set({ service, name, value }))
+		Object.entries(credentials).map(
+			async ([name, value]) => await Bun.secrets.set({ service, name, value })
+		)
 	)
 }
